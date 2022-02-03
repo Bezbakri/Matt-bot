28b1d2ff9c33fee55b1e3651fec6f3f86887dce3
Bezbakri <bezbakri@gmail.com>
2022-02-03 22:39:02 +0530

    Added autoresponder to a cog

---
+++ cogs/AutoResponder.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  3 22:11:01 2022
+
+@author: bezbakri
+"""
+
+import nextcord as discord
+import os
+from nextcord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+BOT_USER_ID=os.getenv("BOT_USER_ID")
+
+class AutoResponder(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.Cog.listener()
+    async def on_message(self,message):
+        if message.author == BOT_USER_ID:
+            return
+        
+        if message.author.id == 316125981725425666:
+            if "gaymers" in message.content.lower():
+                if "morning" in message.content.lower():
+                    await message.channel.send(f"<#{message.channel.id}> is online")
+                    if "good" in message.content.lower():
+                        await message.reply('Good morning furry', mention_author=False)
+                    else:
+                        await message.reply('Morning furry', mention_author=False)
+                    with open("assets/greetings.png", "rb") as fh:
+                        f = discord.File(fh, filename = "greetings.png")
+                    await message.channel.send("*greetings bi furry", file = f)
+                    await message.channel.send(f"{message.author.mention} what's up you meme loving fuck")
+                    await message.channel.send("https://cdn.discordapp.com/attachments/528925999602204681/910120622007418880/video0.mp4")
+                
+                elif "goodnight" in message.content.lower():
+                    await message.channel.send(f"<#{message.channel.id}> is offline")
+        
+        if message.content.lower() == "i live in canada":
+            prev_msg =await message.channel.history(limit = 2).flatten()
+            if prev_msg[1].content.lower() == "wrong":
+                await message.reply("https://en.wikipedia.org/wiki/Kaneda_Castle")
+        
+        
+        
+        await self.bot.process_commands(message)    
+def setup(bot):
+    bot.add_cog(AutoResponder(bot))
--- main.py
+++ main.py
@@ -23,6 +23,25 @@ DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
 bot = commands.Bot(command_prefix = prefix.prefix)
 
+
+
+
+initial_extensions = ['cogs.FunCommands', 
+                      'cogs.ImageCommands', 
+                      'cogs.error_handling', 
+                      'cogs.AutoResponder']
+
+
+
+if __name__ == '__main__':
+    global cogs_added
+    cogs_added = 0
+    for extension in initial_extensions:
+        bot.load_extension(extension)
+        
+        cogs_added+=1
+
+
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
@@ -33,10 +52,7 @@ async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=f"with {len(bot.guilds)} war criminal bunkers"))
     channel = bot.get_channel(924331444661284914)
     await channel.send("I am alive")
-    '''
-    for i in initial_extensions:
-        bot.add_cog(i)
-    await channel.send(f"Added {len(initial_extensions)} cogs {initial_extensions})"'''
+    await channel.send(f"Added {cogs_added} out of {len(initial_extensions)} cogs")
        
 
 
@@ -63,13 +79,7 @@ async def finally_work_pls(ctx):
     await ctx.channel.send("pong")
     
 
-initial_extensions = ['cogs.FunCommands', 'cogs.ImageCommands', 'cogs.error_handling']
 
-
-
-if __name__ == '__main__':
-    for extension in initial_extensions:
-        bot.load_extension(extension)
 
 
 
@@ -109,36 +119,7 @@ async def removing_pog(ctx, cog):
         await ctx.channel.send(f"Added 1 cog {cog}")'''
 
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    
-    if message.author.id == 316125981725425666:
-        if "gaymers" in message.content.lower():
-            if "morning" in message.content.lower():
-                await message.channel.send(f"<#{message.channel.id}> is online")
-                if "good" in message.content.lower():
-                    await message.reply('Good morning furry', mention_author=False)
-                else:
-                    await message.reply('Morning furry', mention_author=False)
-                with open("assets/greetings.png", "rb") as fh:
-                    f = discord.File(fh, filename = "greetings.png")
-                await message.channel.send("*greetings bi furry", file = f)
-                await message.channel.send(f"{message.author.mention} what's up you meme loving fuck")
-                await message.channel.send("https://cdn.discordapp.com/attachments/528925999602204681/910120622007418880/video0.mp4")
-            
-            elif "goodnight" in message.content.lower():
-                await message.channel.send(f"<#{message.channel.id}> is offline")
-    
-    if message.content.lower() == "i live in canada":
-        prev_msg =await message.channel.history(limit = 2).flatten()
-        if prev_msg[1].content.lower() == "wrong":
-            await message.reply("https://en.wikipedia.org/wiki/Kaneda_Castle")
-    
-    
-    
-    await bot.process_commands(message)    
+
  
 
 
