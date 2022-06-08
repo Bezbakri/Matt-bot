dc55896f14d68c1d5c9aabda583217fb0f40a031
Bezbakri <bezbakri@gmail.com>
2022-06-08 23:16:41 +0530

    Wiki command added

--- cogs/WikiCommands.py
+++ cogs/WikiCommands.py
@@ -16,20 +16,34 @@ from bs4 import BeautifulSoup
 load_dotenv()
 
 api_key = os.getenv("GOOGLE_CUSTOM_SEARCH_API_TOKEN")
-cse_token = os.getenv("WIKIPEDIA_SEARCH_ENGINE_ID")
+wiki_cse_token = os.getenv("WIKIPEDIA_SEARCH_ENGINE_ID")
 resource = build("customsearch", 'v1', developerKey=api_key).cse()
 
 def wikisearch(query):
-    search_result = resource.list(q=query, cx=cse_token).execute()['items']
+    search_result = resource.list(q=query, cx=wiki_cse_token).execute()['items']
     wiki_link =  search_result[0]['link']
-    image_link = search_result[0]['pagemap']['metatags'][0]['og:image']
+
     response = requests.get(wiki_link)
     soup = BeautifulSoup(response.text, 'html.parser')
-    wiki_text_whole = soup.findAll('div', attrs = {"class": "mw-parser-output"})
-    wiki_text = wiki_text_whole.find("p")
+    wiki_text_whole = soup.find('div', attrs = {"class": "mw-parser-output"})
+    wiki_text_tags = wiki_text_whole.find_all("p")
+    wiki_text = f"<{wiki_link}>\n```"
+    i = 0
+    for line in wiki_text_tags:
+        if i <5:
+            wiki_text = wiki_text + line.text
+            i+=1
+        else:
+            break
+    if len(wiki_text) > 1825:
+        wiki_text = wiki_text[:1825] + "..."
     
-    print(wiki_text)
-    return image_link
+    try:
+        image_link = search_result[0]['pagemap']['metatags'][0]['og:image']
+        wiki_text = wiki_text + "```\n" + image_link
+    except:
+        wiki_text = wiki_text + "```"
+    return wiki_text
     
     
 
