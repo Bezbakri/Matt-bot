7fcc204e207c042d4da818646dbc2432173db0a0
Bezbakri <bezbakri@gmail.com>
2022-06-11 17:36:16 +0530

    Star Wars wiki command done, trying the keyword wiki search command now

--- cogs/WikiCommands.py
+++ cogs/WikiCommands.py
@@ -71,17 +71,60 @@ class WikiCommands(commands.Cog):
         response = requests.get(wookiee_link)
         soup = BeautifulSoup(response.text, 'html.parser')
         wookiee_text_whole = soup.find('div', attrs = {"class": "mw-parser-output"})
-        wookiee_text_tags = wookiee_text_whole.find_all("p")
+        #wookiee_text_tags = wookiee_text_whole.find_all("p")
         wookiee_text = ""
-        for line in wookiee_text_tags:
-            wookiee_text = wookiee_text + line.text
+        #for line in wookiee_text_tags:
+        #    wookiee_text = wookiee_text + line.text
+        if wookiee_text_whole.find('div', attrs = {"class": "quote"}):
+            for div in wookiee_text_whole.find('div', attrs = {"class": "quote"}):
+                next_node = div.parent
+                next_node_sibling = next_node.find_next_sibling("p")
+            wookiee_text = wookiee_text + next_node_sibling.text
+        elif wookiee_text_whole.find('aside'):
+            for div in wookiee_text_whole.find('aside'):
+                next_node = div.parent
+                next_node_sibling = next_node.find_next_sibling("p")
+            wookiee_text = wookiee_text + next_node_sibling.text
+        else:
+            wookiee_text_tags = wookiee_text_whole.find_all("p")
+            for line in wookiee_text_tags:
+                wookiee_text = wookiee_text + line.text
         
-        if len(wookiee_text)>1500:
-            wookiee_text = wookiee_text[:1500] + "..."
+        
+
+        
+        if len(wookiee_text)>1250:
+            wookiee_text = wookiee_text[:1250] + "..."
         
         embed_color = member_role_color(ctx.author)
         
         wookiee_embed = discord.Embed(title=wookiee_title, url=wookiee_link, description=wookiee_text, color=embed_color)
+        if wookiee_text_whole.find('aside'):
+            aside_content =  wookiee_text_whole.find('aside').strings
+            sub_text = ""
+            for line in aside_content:
+                if line[0] == "[":
+                    pass
+                elif wookiee_title.partition("|")[0].strip() in line:
+                    pass
+                else:
+                    sub_text = sub_text + line
+            if len(sub_text) > 1021:
+                sub_text = sub_text[:1021] + "..."
+            wookiee_embed.add_field(name = "Some Information", value = sub_text)
+        if wookiee_text_whole.find('table', attrs = {"class": "scrollbox"}) and wookiee_text_whole.find('table', attrs = {"class": "scrollbox"}).parent.find_previous_sibling("h2").text == "Appearances[]":
+            table = wookiee_text_whole.find('table', attrs = {"class": "scrollbox"})
+            ul = table.find("ul")
+            appearances = ul.find_all("li")
+            number_of_appearances = len(appearances)
+            wookiee_embed.add_field(name = "Number of Appearances", value = number_of_appearances)
+        elif wookiee_text_whole.find('span', text = "Appearances"):
+            first_appearance = wookiee_text_whole.find('small', text = "(First appearance)")
+            ul = first_appearance.parent.parent
+            appearances = ul.find_all("li")
+            number_of_appearances = len(appearances)
+            wookiee_embed.add_field(name = "Number of Appearances", value = number_of_appearances)
+            
         wookiee_embed.set_footer(text = f"Requested by {ctx.author.display_name}", icon_url = ctx.author.avatar.url)
         
         try:
@@ -90,6 +133,34 @@ class WikiCommands(commands.Cog):
         except:
             pass
         await ctx.send(embed=wookiee_embed)
+    @commands.command()
+    async def wookie(self, ctx):
+        "Respect the double E."
+        await ctx.reply("Respect the double E.")
+    @commands.command(aliases = ['wikifind', ])
+    async def specificwikisearch(self, ctx, article, keyword):
+        "Searches for a certain header in the wikipedia article you want to search up"
+        search_result = resource.list(q=article, cx=wiki_cse_token).execute()['items']
+        wiki_link =  search_result[0]['link']
+        await ctx.send(f"<{wiki_link}>")
+        
+        response = requests.get(wiki_link)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        wiki_text = soup.find('div', attrs = {"class": "mw-parser-output"})
+        
+        try:
+            text_to_send = ""
+            for header in wiki_text.find_all('h3'):
+                if keyword.lower() in header.text.lower():
+                    while True:
+                        para_text = header.find_next_sibling("p")
+                        if para_text == None:
+                            break
+                        text_to_send+=para_text
+                    await ctx.send(text_to_send)
+        except:
+            await ctx.send("The topic you were looking for wasn't found.")
+        
         
 def setup(bot):
     bot.add_cog(WikiCommands(bot))
