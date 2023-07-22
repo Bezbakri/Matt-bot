a81de31d50ae3a1df837f8fe768ba757f85a2544
Bezbakri <bezbakri@gmail.com>
2023-07-22 13:01:07 +0530

    Had to make sure that the bot can't reply to other bots because SOMEONE couldn't stop spamming it

--- cogs/AutoResponder.py
+++ cogs/AutoResponder.py
@@ -58,7 +58,7 @@ class AutoResponder(commands.Cog):
         
     @commands.Cog.listener()
     async def on_message(self,message):
-        if message.author == self.bot.user:
+        if message.author == self.bot.user or message.author.bot:
             return
         else:
             for i in self.bot.bad_words_list:
